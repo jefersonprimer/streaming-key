0dca658347ca6b88743415c9262e8038137ceaae
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-26 17:10:58 -0300

    feat: routes GET and POST

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import "github.com/labstack/echo/v4"
+
 func main() {
-	fmt.Println("hello")
+	e := echo.New()
+
+	e.POST("/auth", fun(c echo.Context) error {
+		log.Default().Println("Running Auth...")
+		body := c.Request().body
+		defer body.Close()
+
+		fields, _ := io.ReadAll(body)
+		fmt.Println(string(fields))
+		
+		return c.String(http.StatusOK, "WORKING")
+	})
+
+	e.GET("/healthcheck", fun(c echo.Context) error {
+		return c.String(http.StatusOK, "WORKING")
+	})
+
+	e.Logger.Fatal(e.Start(":8000"))
 }
